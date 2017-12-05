9d9a92e250d6a63bb809ffb4bbf64f93afc09572
Dion <d223013@edu.rocwb.nl>
2017-12-05 09:56:54 +0100

    Index redesign

--- laravel/resources/views/admin/index.blade.php
+++ laravel/resources/views/admin/index.blade.php
@@ -7,6 +7,102 @@
 @section('content')
     <div class="container-fluid userinfo">
         <div class="container">
+
+            <div class="container indexTitle col-12">
+                <h2>USERINFORMATION</h2>
+            </div>
+            <div class="row row-sizer-userinfo">
+                <div class="col-lg-2"></div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Amount Of Products</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-shopping-bag justified-content-center" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Amount Of Users</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Amount Of Orders</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-barcode" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Total Vouchers Used</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container indexTitle col-12">
+                <h2>MOST POPULAR CLOTHING</h2>
+            </div>
+            <div class="row row-sizer-userinfo">
+                <div class="col-lg-2"></div>
+                <div class=" col-lg-2 col-md-3">
+                    <div class="item-info">
+                        <div class="d-inline">
+
+                        <h2>title</h2>
+
+                        </div>
+
+                        <div class="d-inline">
+                        <p>description</p>
+
+                        </div>
+                        <p>more Description</p>
+                    </div>
+                </div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Amount Of Users</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Amount Of Orders</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-barcode" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+                <div class="item col-lg-2 col-md-3">
+                    <div class="item-info text-center">
+                        <span>Total Vouchers Used</span>
+                        <div class="mw-100"></div>
+                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                        <div class="mw-100"></div>
+                        <span>0</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container indexTitle col-12">
+                <h2>LOW ON STOCK</h2>
+            </div>
             <div class="row row-sizer-userinfo">
                 <div class="col-lg-2"></div>
                 <div class="item col-lg-2 col-md-3">
@@ -48,129 +144,9 @@
                 </div>
             </div>
 
-            <div class="row row-sizer-userinfo">
-                <div class="col-lg-2"></div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Products</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-shopping-bag justified-content-center" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Users</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-users" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Orders</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-barcode" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Total Vouchers Used</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
+            <div class="container indexTitle col-12">
+                <h2>MOST LOGINS FROM HOUSES</h2>
             </div>
-
-            <div class="row row-sizer-userinfo">
-                <div class="col-lg-2"></div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Products</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-shopping-bag justified-content-center" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Users</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-users" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Orders</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-barcode" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Total Vouchers Used</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row row-sizer-userinfo">
-                <div class="col-lg-2"></div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Products</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-shopping-bag justified-content-center" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Users</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-users" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Amount Of Orders</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-barcode" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-
-                <div class="item col-lg-2 col-md-3">
-                    <div class="item-info text-center">
-                        <span>Total Vouchers Used</span>
-                        <div class="mw-100"></div>
-                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                        <div class="mw-100"></div>
-                        <span>0</span>
-                    </div>
-                </div>
-            </div>
-
             <div class="row row-sizer-userinfo">
                 <div class="col-lg-2"></div>
                 <div class="item col-lg-2 col-md-3">
