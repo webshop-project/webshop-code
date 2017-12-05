c6d6ad6bd8660cda6613477845560b8216b1b4b6
Unknown <lk98350@edu.rocwb.nl>
2017-12-05 12:15:02 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ laravel/app/brand_models.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class brand_model extends Model
+{
+    //
+}
--- laravel/database/migrations/2017_12_05_103658_create_brand_models_table.php
+++ laravel/database/migrations/2017_12_05_103658_create_brand_models_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHousesTable extends Migration
+class CreateBrandModelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateHousesTable extends Migration
      */
     public function up()
     {
-        Schema::create('houses', function (Blueprint $table) {
+        Schema::create('brand_models', function (Blueprint $table) {
             $table->increments('id');
-            
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateHousesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('houses');
+        Schema::dropIfExists('brand_models');
     }
 }
