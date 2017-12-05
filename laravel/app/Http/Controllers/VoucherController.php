fde07632b70e729770b0b119465b53e2b36c2f0d
Dion <d223013@edu.rocwb.nl>
2017-12-05 11:26:05 +0100

    web adjust

--- laravel/app/Http/Controllers/VoucherController.php
+++ laravel/app/Http/Controllers/VoucherController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\voucher;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class VoucherController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('admin/product');
+        //
     }
 
     /**
@@ -23,7 +24,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        return view('admin/productAdd');
+        //
     }
 
     /**
@@ -40,10 +41,10 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\voucher  $voucher
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(voucher $voucher)
     {
         //
     }
@@ -51,10 +52,10 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\voucher  $voucher
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(voucher $voucher)
     {
         //
     }
@@ -63,10 +64,10 @@ class ProductController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\voucher  $voucher
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, voucher $voucher)
     {
         //
     }
@@ -74,10 +75,10 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\voucher  $voucher
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(voucher $voucher)
     {
         //
     }
