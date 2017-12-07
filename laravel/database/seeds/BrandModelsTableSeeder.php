59b9cc854a484843c88b066b31886f08d176190a
Unknown <lk98350@edu.rocwb.nl>
2017-12-07 09:47:31 +0100

    laravel db 1.5

--- laravel/database/seeds/BrandModelsTableSeeder.php
+++ laravel/database/seeds/BrandModelsTableSeeder.php
@@ -11,6 +11,23 @@ class BrandModelsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $carbon = new Carbon();
+        $modelsList = ['7S','8S','Note','7S','X'];
+        $j = 1;
+        for( $i = 0 ; $i < count($modelsList) ; $i++ )
+        {
+            if($i > 2)
+            {
+                $j=2;
+            }
+            DB::table('brand_models')->insert
+            (
+                [
+                    'brand_id' => $j,
+                    'modelName' => $modelsList[$i],
+                    'created_at' => $carbon,
+                ]
+            );
+        }
     }
 }
