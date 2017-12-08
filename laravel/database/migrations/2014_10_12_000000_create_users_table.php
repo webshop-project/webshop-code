2e1d68347f455929ec0ec51338ea8386471f7c25
Unknown <lk98350@edu.rocwb.nl>
2017-12-08 09:24:17 +0100

    laravel db 1.9
    decrease seeder amount products, add amountOfLogin in tbl_users, add discount in tbl_products

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('amountOfLogin');
             $table->string('loginToken');
             $table->string('loginName');
             $table->string('password');
