d7c9eaa1ef4b082e95594552fa5cf24491c4704e
Titson <tomaszt@hotmail.nl>
2017-12-12 12:15:37 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit de11ca8b9ba4493d4d8185366c50c92c81ce51f3, reversing
    changes made to 058adb2df8ed4b8ba19027c4a07e0230f7685334.

--- laravel/app/Mail/Vouchers.php
+++ laravel/app/Mail/Vouchers.php
@@ -1,1 +1,33 @@
-        return $this->from('amowebshop@radiuscollege.nl')->view('email.voucher');
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Vouchers extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
--- laravel/resources/views/email/voucher.blade.php
+++ laravel/resources/views/email/voucher.blade.php
@@ -8,11 +8,6 @@
     <title>Document</title>
 </head>
 <body>
-<<<<<<< HEAD
 
-=======
-    <h1>Test</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consectetur consequatur distinctio eaque facere iste magnam mollitia natus praesentium quibusdam sequi, ut. Aliquid at et labore nam officia unde!</p>
->>>>>>> master
 </body>
 </html>
