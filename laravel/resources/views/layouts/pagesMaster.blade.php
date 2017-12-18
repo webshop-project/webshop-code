8d8d8a36536b1cb5c59fd7cd6b7ae63c654ba257
bp153373 <bp153373@edu.rocwb.nl>
2017-12-18 10:55:57 +0100

    Cart WIP

--- laravel/resources/views/layouts/pagesMaster.blade.php
+++ laravel/resources/views/layouts/pagesMaster.blade.php
@@ -199,7 +199,33 @@
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-@yield('cartupdate')
+<script>
+    (function(){
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $('.quantity').on('change', function() {
+            var id = $(this).attr('data-id')
+            $.ajax({
+                type: "PATCH",
+                url: '{{ url("/cart") }}' + '/' + id,
+                data: {
+                    'quantity': this.value,
+                },
+                success: function(data) {
+                    window.location.href = '{{ url('/cart') }}';
+                }
+            });
+
+        });
+
+    })();
+
+</script>
 <script>
     function openNav() {
         document.getElementById("myShoppingCart").style.width = "550px";
