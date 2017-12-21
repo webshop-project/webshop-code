b4004278ba862384a0e1f3211d5fd80ba58e84f1
Unknown <lk98350@edu.rocwb.nl>
2017-12-21 11:08:54 +0100

    db testing

--- laravel/database/seeds/WarehouseModelsTableSeeder.php
+++ laravel/database/seeds/WarehouseModelsTableSeeder.php
@@ -36,7 +36,7 @@ class WarehouseModelsTableSeeder extends Seeder
                                 'price'         => random_int(5,25),
                                 'viewAmount'    => random_int(5, 120),
                                 'img'           => $faker->imageUrl($width = 500, $height = 650),
-                                'description' => $faker->sentence(2),
+                                'description'   => $faker->sentence(2),
                             ]
                         );
                     }
@@ -73,7 +73,7 @@ class WarehouseModelsTableSeeder extends Seeder
                                 'price'         => random_int(5,25),
                                 'viewAmount'    => random_int(5, 120),
                                 'img'           => $faker->imageUrl($width = 500, $height = 650),
-                                'description' => $faker->sentence(2),
+                                'description'   => $faker->sentence(2),
                             ]
                         );
                     }
@@ -87,7 +87,7 @@ class WarehouseModelsTableSeeder extends Seeder
                             'price'         => random_int(5,25),
                             'viewAmount'    => random_int(5, 120),
                             'img'           => $faker->imageUrl($width = 500, $height = 650),
-                            'description' => $faker->sentence(2),
+                            'description'   => $faker->sentence(2),
                         ]
                     );
                 }
