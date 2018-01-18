ce2e00a7f4c714336c20f562a8de8d0ab16fcf64
Unknown <d233407@edu.rocwb.nl>
2018-01-18 09:59:33 +0100

    Shop Images

--- laravel/resources/views/pages/shop.blade.php
+++ laravel/resources/views/pages/shop.blade.php
@@ -47,34 +47,34 @@
                     <div class="row">
                         @foreach($products as $product)
                             <div class="house{{$product->house->id}} category{{$product->category->id}} display-block">
-                            <div class="block span3">
-                                <div class="product">
-                                        <img src="{{$product->img}}" alt="{{$product->house->name}} {{$product->category->name}}">
-                                    <div class="buttons">
-                                        <a class="preview btn btn-large btn-info" href="{{action('WarehouseController@show',$product->id)}}"><i class="glyphicon glyphicon-eye-open"></i> View item</a>
+                                <div class="block span3">
+                                    <div class="product row justify-content-center">
+                                            <img src="{{$product->img}}" alt="{{$product->house->name}} {{$product->category->name}}">
+                                        <div class="buttons">
+                                            <a class="preview btn btn-large btn-info" href="{{action('WarehouseController@show',$product->id)}}"><i class="glyphicon glyphicon-eye-open"></i> View item</a>
+                                        </div>
+                                    </div>
+                                    <div class="info">
+                                        <h4 onclick="count({{$product->id}})">{{$product->house->name}}<br> {{$product->category->name}}
+                                        @if(!empty($product->brandModel->name))
+                                            {{$product->brandModel->name}}
+                                        @endif
+                                        </h4>
+                                        @foreach($product->warehouse as $price)
+                                            @if($loop->first)
+                                                <span class="price">€{{number_format($price->price,2,',',' ')}}</span>
+                                            @endif
+                                        @endforeach
+                                        <form action="{{ url('/shop/cart') }}" method="POST">
+                                            {{csrf_field()}}
+                                            <input type="hidden" name="id" value="{{$product->id}}">
+                                            <input type="hidden" name="name" value="{{$product->house->name}} {{$product->category->name}}">
+                                            <input type="hidden" name="qty" value="{{$product->size}}">
+                                            <input type="hidden" name="price" value="{{$product->warehouse[0]->price}}">
+                                            <input type="submit" class="btn btn-info pull-right" value="Add to Cart">
+                                        </form>
                                     </div>
                                 </div>
-                                <div class="info">
-                                    <h4 onclick="count({{$product->id}})">{{$product->house->name}}<br> {{$product->category->name}}
-                                    @if(!empty($product->brandModel->name))
-                                        {{$product->brandModel->name}}
-                                    @endif
-                                    </h4>
-                                    @foreach($product->warehouse as $price)
-                                        @if($loop->first)
-                                            <span class="price">€{{number_format($price->price,2,',',' ')}}</span>
-                                        @endif
-                                    @endforeach
-                                    <form action="{{ url('/shop/cart') }}" method="POST">
-                                        {{csrf_field()}}
-                                        <input type="hidden" name="id" value="{{$product->id}}">
-                                        <input type="hidden" name="name" value="{{$product->house->name}} {{$product->category->name}}">
-                                        <input type="hidden" name="qty" value="{{$product->size}}">
-                                        <input type="hidden" name="price" value="{{$product->warehouse[0]->price}}">
-                                        <input type="submit" class="btn btn-info pull-right" value="Add to Cart">
-                                    </form>
-                                </div>
-                            </div>
                             </div>
                         @endforeach
                     </div>
