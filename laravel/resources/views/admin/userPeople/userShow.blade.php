3602819e33b906967851a679647aa6beffd47c3b
d201670 <d201670@edu.rocwb.nl>
2018-01-19 10:06:57 +0100

    Users
    User show admin
    User edit admin
    User edit user

--- laravel/resources/views/admin/userPeople/userShow.blade.php
+++ laravel/resources/views/admin/userPeople/userShow.blade.php
@@ -13,14 +13,18 @@
                     <img style="max-width: 300px" src="/img/userImage.png" alt="">
                 </div>
                 <div class="col-5 img-preview-left">
+                    @foreach($users as $user)
                     <p><b>Name:</b></p>
                     <span>{{$user->name}}</span>
                     <p><b>Address:</b></p>
                     <span>{{$user->streetName}} {{$user->houseNumber}}</span>
+                        <p><b>Address Add-on:</b></p>
+                        <span>{{$user->houseNumberAdd}}</span>
                     <p><b>Zipcode:</b></p>
                     <span>{{$user->zipcode}}</span>
                     <p><b>Created at:</b></p>
                     <span>{{$user->created_at}}</span>
+                    @endforeach
                 </div>
             </div>
         </div>
---
+++ laravel/resources/views/pages/userEdit.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts/pagesMaster')
+
+@section('title')
+    User Settings
+@endsection
+
+
+
+
+@section('content')
+
+
+
+
+    <div class="">
+        <div class="user-info">
+            <div class="form-inline">
+                <div class="col-3">
+                    <img style="max-width: 300px" src="/img/userImage.png" alt="">
+                </div>
+                <div class="col-5 img-preview-left">
+
+
+                    <p><b>Name:</b></p>
+                    <span>{{$user->name}}</span>
+
+                    <form action="{{action('UserController@update', $user->id)}}" method="POST" >
+                        {{csrf_field()}}
+                        {{method_field('PUT')}}
+                        <div class="form-group form-padding">
+                            <p>id 1 = Viking, id 2 = Dragon, id 3 = Raven, id 4 = Serpent.</p>
+                        </div>
+                        <div class="form-group form-padding">
+                            <label for="house_id">House :</label>
+                            <input class="form-control col" type="number" name="house_id" value="{{$user->houseId}}">
+                        </div>
+                        <div class="form-group form-padding">
+                            <label for="streetName">Streetname :</label>
+                            <input class="form-control col" type="text" name=streetName" value="{{$user->streetName}}">
+                        </div>
+                        <div class="form-group form-padding">
+                            <label for="houseNumber">Housenumber :</label>
+                            <input class="form-control col" type="number" name="houseNumber" value="{{$user->houseNumber}}">
+                        </div>
+                        <div class="form-group form-padding">
+                            <label for="houseNumberAdd">Housenumber Addon :</label>
+                            <input class="form-control col" type="text" name="houseNumberAdd" value="{{$user->houseNumberAdd}}">
+                        </div>
+                        <div class="form-group form-padding">
+                            <label for="zipcode">Zipcode :</label>
+                            <input class="form-control col" type="text" name="zipcode" value="{{$user->zipcode}}">
+                        </div>
+                        <div class="form-group form-padding">
+                            <input type="submit" class="btn btn-success" value="submit" />
+                        </div>
+                        @if(Auth::user()->firstLogin == 0)
+                                {{ user()->firstLogin + 1 }}
+                        @endif
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
